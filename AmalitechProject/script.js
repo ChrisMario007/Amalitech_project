952a8495dfa77d366699da50e672db08235dcc9d
ChrisMario007 <crismario60z6@gmail.com>
2023-07-13 07:45:15 +0000

    body+darkmode

--- AmalitechProject/script.js
+++ AmalitechProject/script.js
@@ -1,11 +1,24 @@
+// const toggleButton = document.getElementById('dark-mode-toggle');
+// const generalBox = document.querySelector('.general-box');
+
+// function toggleDarkMode() {
+//   if (toggleButton.checked) {
+//     generalBox.classList.add('dark-mode');
+//   } else {
+//     generalBox.classList.remove('dark-mode');
+//   }
+// }
+
+// toggleButton.addEventListener('change', toggleDarkMode);
+
 const toggleButton = document.getElementById('dark-mode-toggle');
-const generalBox = document.querySelector('.general-box');
+const body = document.body;
 
 function toggleDarkMode() {
   if (toggleButton.checked) {
-    generalBox.classList.add('dark-mode');
+    body.classList.add('dark-mode');
   } else {
-    generalBox.classList.remove('dark-mode');
+    body.classList.remove('dark-mode');
   }
 }
 
